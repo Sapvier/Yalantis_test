03af2d65f37341f55109d2717a5c72f17db31497
Langem <horzn1@gmail.com>
2020-11-24 23:10:18 +0200

    v3 w/ prettier

--- src/components/CheckedUser.js
+++ src/components/CheckedUser.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CheckedUser = ({user, month}) => {
     return (
-        <div>{user.lastName} {user.firstName} - {user.dob.slice(6, 7)} {month}, {user.dob.slice(0, 4)} year</div>
+        <div>&#8226; {user.lastName} {user.firstName} - {parseInt(user.dob.slice(5, 7))} {month}, {user.dob.slice(0, 4)} year</div>
     )
 }
 
